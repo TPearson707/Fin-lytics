6860f159289049a546d5d88771b29afad819811f
adam plankey <adameplankey@gmail.com>
2025-12-09 15:27:53 -0500

    stripe and admin routes

--- back_end/main.py
+++ back_end/main.py
@@ -25,6 +25,7 @@ import balance_routes
 import budget_goals
 import stripe_routes
 import algorithm_marketplace_routes
+import admin_routes
 from startup import initialize_prediction_service, cleanup_prediction_service
 
 @asynccontextmanager
@@ -67,6 +68,7 @@ app.include_router(balance_routes.router)
 app.include_router(budget_goals.router)
 app.include_router(stripe_routes.router)
 app.include_router(algorithm_marketplace_routes.router)
+app.include_router(admin_routes.router)
 
 
 # Create MySQL tables (make sure this is called at least once)
